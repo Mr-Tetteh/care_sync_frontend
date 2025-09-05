1f10ba01236561c14e5f3f1bc0b37bbc6654b323
Mr. Tetteh <danielstay73@gmail.com>
2025-09-05 02:50:25 +0000

    payment part 2

--- composables/usePayments.ts
+++ composables/usePayments.ts
@@ -1,9 +1,12 @@
+import {useAuth} from "~/composables/UseAuth";
+
 export const usePayments = () => {
     interface Payment {
         id?: number
         reason_for_payment?: string
         insurance_cover?: string
         price: number
+        lab: number,
         NHIS: boolean
     }
 
@@ -16,12 +19,18 @@ export const usePayments = () => {
 
     const consultationTrue = ref<Payment | null>(null)
     const consultationFalse = ref<Payment | null>(null)
+    const labsTrue = ref<Payment | null>(null)
+    const labsFalse = ref<Payment | null>(null)
+
 
     const consultationPaymentsTrue = async () => {
         try {
             const {data} = await useFetch(useRuntimeConfig().public.api + '/hospital-service/nhis/true', {
                 method: 'GET',
-                headers: {'Content-Type': 'application/json'},
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${useAuth().authToken.value}`,
+                },
             })
             consultationTrue.value = data.value // take first match
             return consultationTrue.value
@@ -34,7 +43,11 @@ export const usePayments = () => {
         try {
             const {data} = await useFetch(useRuntimeConfig().public.api + '/hospital-service/nhis/false', {
                 method: 'GET',
-                headers: {'Content-Type': 'application/json'},
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${useAuth().authToken.value}`,
+                },
+
             })
             consultationFalse.value = data.value
             return consultationFalse.value
@@ -43,11 +56,48 @@ export const usePayments = () => {
         }
     }
 
+    const LabsTrue = async () => {
+        try {
+            const {data} = await useFetch(useRuntimeConfig().public.api + '/lab-service/nhis/true', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${useAuth().authToken.value}`,
+                },
+
+            })
+            labsTrue.value = data.value // take first match
+            return consultationTrue.value
+        } catch (error) {
+            console.error('Error fetching consultation payments with NHIS cover:', error)
+        }
+    }
+
+    const LabsFalse = async () => {
+        try {
+            const {data} = await useFetch(useRuntimeConfig().public.api + '/lab-service/nhis/false', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${useAuth().authToken.value}`,
+                },
+            })
+            labsFalse.value = data.value // take first match
+            return consultationTrue.value
+        } catch (error) {
+            console.error('Error fetching consultation payments with NHIS cover:', error)
+        }
+    }
+
     return {
         consultationTrue,
         consultationFalse,
         consultationPaymentsTrue,
         consultationPaymentsFalse,
-        Payments
+        Payments,
+        labsTrue,
+        labsFalse,
+        LabsTrue,
+        LabsFalse
     }
 }
--- pages/patients/patients.vue
+++ pages/patients/patients.vue
@@ -142,7 +142,7 @@ const filteredPatients = computed(() => {
                     Birth
                   </th>
                   <th class="px-6 py-4 text-left text-xs font-semibold text-slate-600 uppercase tracking-wider"
-                      v-if="user.role === 'Nurse' || user.role === 'Doctor' || user.role === 'Pharmacist' || user.role === 'Lab Technician'">
+                      v-if="user.role === 'Nurse' || user.role === 'Doctor' || user.role === 'Pharmacist' || user.role === 'Lab Technician' || user.role === 'Accountant'">
                     Actions
                   </th>
                 </tr>
@@ -170,7 +170,7 @@ const filteredPatients = computed(() => {
                     <div class="text-sm text-slate-900">{{ item.patient_id }}</div>
                   </td>
                   <td class="px-6 py-4 whitespace-nowrap">
-                    <div class="text-sm text-slate-900">{{ item.ghana_card_number}}</div>
+                    <div class="text-sm text-slate-900">{{ item.ghana_card_number }}</div>
                   </td>
                   <td class="px-6 py-4 whitespace-nowrap">
                     <div class="text-sm text-slate-900">{{ item.phone }}</div>
@@ -205,7 +205,7 @@ const filteredPatients = computed(() => {
                       <NuxtLink
                           :to="`/patients/past_record/${item.id}`"
                           class="inline-flex items-center gap-1 px-3 py-2 text-sm font-medium text-white bg-gradient-to-r from-emerald-500 to-emerald-600 hover:from-emerald-600 hover:to-emerald-700 rounded-lg shadow-sm transition-all duration-200 transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-offset-2"
-                          v-if="user.role === 'Nurse' || user.role === 'Doctor' || user.role === 'Pharmacist' || user.role === 'Lab Technician'"
+                          v-if="user.role === 'Nurse' || user.role === 'Doctor' || user.role === 'Pharmacist' || user.role === 'Lab Technician'|| user.role === 'Accountant'"
                       >
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                              stroke="currentColor" class="w-4 h-4">
